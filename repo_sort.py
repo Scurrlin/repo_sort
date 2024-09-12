fae2688648677ec33aa79a0ae3fd1db7b649cb12
Sean Currlin <sean.currlin@gmail.com>
2024-09-11 23:54:46 -0400

    final version

--- repo_sort.py
+++ repo_sort.py
@@ -44,6 +44,8 @@ sorted_repos = sorted(all_repos, key=itemgetter('created_at'), reverse=True)
 
 # Static README content you want to keep above your repo list
 readme_content = """
+<a name="top"></a>
+
 # Hi, I'm Sean 👋
 
 <table>
@@ -61,7 +63,6 @@ I have a demonstrated proficiency in software development, with a proven track r
 ### [Skip to Contributions](#contributions)
 
 ### Repositories sorted by date created:
-
 """
 
 repos_per_page = 30
@@ -88,13 +89,18 @@ for page_num in range(total_pages):
         # Add the repository to the README content
         readme_content += f"### [{repo['name']}]({repo['html_url']})\n"
         readme_content += f"{language_color} {language} • Created on {formatted_date}\n\n"
-        
+
         # Omit separator if it's the last repository on the page
         if index < len(page_repos) - 1:
             readme_content += "---\n\n"
 
 # Add an anchor tag at the end for "Skip to Contributions"
 readme_content += "\n<a name='contributions'></a>\n"
+
+# Add the "Back to Top" link at the bottom
+readme_content += """
+### [Back to Top](#top)
+"""
 
 # Write the generated content to the README.md file
 with open("README.md", "w") as readme_file:
